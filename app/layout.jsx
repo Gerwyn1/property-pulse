630b32207465b26e97199d39db854d7b89d389f7
Gerwyn Sim <gerwynsim@hotmail.com>
2024-04-01 01:07:03 +0800

    first commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,22 +1,28 @@
-import '@/assets/styles/globals.css';
-import Footer from '@/components/Footer';
-import Navbar from '@/components/Navbar';
+import "@/assets/styles/globals.css";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import AuthProvider from "@/components/AuthProvider";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const metadata = {
-  title: 'PropertyPulse | Find The Perfect Rental',
-  description: 'Find your dream rental property',
-  keywords: 'rental, find rentals, find properties'
-}
+  title: "PropertyPulse | Find The Perfect Rental",
+  description: "Find your dream rental property",
+  keywords: "rental, find rentals, find properties",
+};
 
 const MainLayout = ({ children }) => {
   return (
-    <html lang="en">
-      <body>
-        <Navbar/>
-        <div>{children}</div>
-        <Footer/>
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body>
+          <Navbar />
+          <div>{children}</div>
+          <Footer />
+          <ToastContainer/>
+        </body>
+      </html>
+    </AuthProvider>
   );
 };
 
---
+++ utils/authOptions.js
@@ -0,0 +1,50 @@
+import GoogleProvider from "next-auth/providers/google";
+import connectDB from "@/config/database";
+import User from "@/models/User";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    // Invoked on successful signin
+    async signIn({ profile }) {
+      // 1. connect to db
+      await connectDB();
+      // 2. check if user exists
+      const userExists = await User.findOne({ email: profile.email });
+      // 3. if not, add user to db
+      if (!userExists) {
+        // Truncate user name if  it's too long
+        const username = profile.name.slice(0, 20);
+
+        await User.create({
+          email: profile.email,
+          username,
+          image: profile.picture,
+        });
+      }
+      // 4. return true to allow sign in
+      return true;
+    },
+    // Modifies the session object
+    async session({ session }) {
+      // 1. get user from db (the user that signs in)
+      const user = await User.findOne({ email: session.user.email });
+      // 2. assign the user id to the session
+      session.user.id = user._id.toString();
+      // 3. return session
+      return session;
+    },
+  },
+};
